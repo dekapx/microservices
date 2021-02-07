243c906123fcf1dad6003447e34755c583a1de2c
KAPIL <dekapx@MacBook-Pro.local>
2021-02-07 18:36:41 +0000

    eureka discovery service

---
+++ user-service/src/main/java/com/dekapx/apps/web/core/controller/aspect/RestControllerExceptionHandlerAdvice.java
@@ -0,0 +1,7 @@
+package com.dekapx.apps.web.core.controller.aspect;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class RestControllerExceptionHandlerAdvice {
+}
