4adf95a72aa011d5b0fbf0b84050c5e3912a797b
KAPIL <dekapx@MacBook-Pro.local>
2021-03-22 22:27:13 +0000

    implement mapstruct

--- contact-service/src/main/java/com/dekapx/apps/contact/model/ContactDto.java
+++ contact-service/src/main/java/com/dekapx/apps/contact/model/ContactDto.java
@@ -1,4 +1,20 @@
 package com.dekapx.apps.contact.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContactDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
 }
--- contact-service/src/main/java/com/dekapx/apps/contact/service/ContactServiceImpl.java
+++ contact-service/src/main/java/com/dekapx/apps/contact/service/ContactServiceImpl.java
@@ -1,15 +1,21 @@
 package com.dekapx.apps.contact.service;
 
+import com.dekapx.apps.contact.mapper.ContactMapper;
 import com.dekapx.apps.contact.model.ContactDto;
+import com.dekapx.apps.contact.repository.ContactRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
-@Transactional(propagation = Propagation.REQUIRES_NEW)
+@Transactional
+@RequiredArgsConstructor
 public class ContactServiceImpl implements ContactService {
+    private ContactMapper mapper;
+    private ContactRepository repository;
+
     @Override
     public void save(final ContactDto dto) {
 
