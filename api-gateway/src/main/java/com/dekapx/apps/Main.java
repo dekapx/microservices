f85b9e49adfc08c6317c44e932ef0c98f9d10cde
KAPIL <dekapx@MacBook-Pro.local>
2021-04-19 23:01:48 +0100

    api gateway and name refactoring

--- api-gateway/src/main/java/com/dekapx/apps/Main.java
+++ api-gateway/src/main/java/com/dekapx/apps/Main.java
@@ -2,12 +2,12 @@ package com.dekapx.apps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaServer
+@EnableEurekaClient
 @SpringBootApplication
-public class AppMain {
+public class Main {
 	public static void main(String[] args) {
-		SpringApplication.run(AppMain.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 }
--- user-service/src/main/java/com/dekapx/apps/Main.java
+++ user-service/src/main/java/com/dekapx/apps/Main.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class AppMain {
+public class Main {
 	public static void main(String[] args) {
-		SpringApplication.run(AppMain.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 }
